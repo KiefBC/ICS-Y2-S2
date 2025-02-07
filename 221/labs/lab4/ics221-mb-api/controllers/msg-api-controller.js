a362186774fc03ba9f530a57c8c18078e0b01ae2
KiefBC <kiefhay642@outlook.com>
2025-02-06 17:33:52 -0800

    PATCH and DELETE working

--- 221/labs/lab4/ics221-mb-api/controllers/msg-api-controller.js
+++ 221/labs/lab4/ics221-mb-api/controllers/msg-api-controller.js
@@ -1,3 +1,5 @@
+import messageSchema from '../models/message-schema.js';
+
 let messages = [];
 
 // GET Request Handler
@@ -7,16 +9,59 @@ const getAllMessages = (req, res) => {
     } catch (err) {
         res.status(400).send('Unknown Error GET');
     }
+
+    console.log(messages);
 };
 
 // POST Request Handler
 const addNewMessage = (req, res) => {
     try {
-        messages.push(req.body);
-        res.status(200).json(messages);
+        let message = messageSchema.parse(req.body);
+        message.id = messages.length + 1;
+        messages.unshift(message);
+        res.status(201).json(message);
     } catch (err) {
-        res.status(400).send('Unknown Error POST');
+        console.error('Error adding message:', err);
+        res.status(400).send('Bad Request. The message in the body of the \
+            Request is either missing or malformed.');
     }
+
+    console.log(messages);
 };
 
-export default { getAllMessages, addNewMessage };
+// PATCH Request Handler
+const updateMessageById = (req, res) => {
+    try {
+        const { id } = req.params;
+        const { text } = req.body;
+        const message = messages.find(msg => msg.id === parseInt(id));
+        if (!message) {
+            return res.status(404).send('Message not found');
+        }
+        message.text = text;
+        res.status(200).json(message);
+    } catch (err) {
+        res.status(400).send('Unknown Error PATCH');
+    }
+
+    console.log(messages);
+};
+
+// DELETE Request Handler
+const deleteMessageById = (req, res) => {
+    try {
+        const { id } = req.params;
+        const messageIndex = messages.findIndex(msg => msg.id === parseInt(id));
+        if (messageIndex === -1) {
+            return res.status(404).send('Message not found');
+        }
+        const deletedMessage = messages.splice(messageIndex, 1)[0];
+        res.status(200).json(deletedMessage);
+    } catch (err) {
+        res.status(400).send('Unknown Error DELETE');
+    }
+
+    console.log(messages);
+};
+
+export default { getAllMessages, addNewMessage, updateMessageById, deleteMessageById };
--- 221/labs/lab4/ics221-mb-api/routes/api-router.js
+++ 221/labs/lab4/ics221-mb-api/routes/api-router.js
@@ -1,7 +1,14 @@
 import express from 'express';
 import msgAPIController from '../controllers/msg-api-controller.js';
+
 const router = express.Router();
+
 router.route('/messages')
     .get(msgAPIController.getAllMessages)
     .post(msgAPIController.addNewMessage);
+    
+router.route('/messages/:id')
+    .patch(msgAPIController.updateMessageById)
+    .delete(msgAPIController.deleteMessageById);
+
 export default router;
