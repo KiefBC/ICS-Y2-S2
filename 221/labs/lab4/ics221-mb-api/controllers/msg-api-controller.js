e217763c03abecb562ec5cf70df0402c16f970ca
KiefBC <kiefhay642@outlook.com>
2025-02-06 16:55:10 -0800

    Task 5 done

--- 221/labs/lab4/ics221-mb-api/controllers/msg-api-controller.js
+++ 221/labs/lab4/ics221-mb-api/controllers/msg-api-controller.js
@@ -1,11 +1,22 @@
+let messages = [];
+
 // GET Request Handler
 const getAllMessages = (req, res) => {
-    res.status(200).send('Successful API GET Request');
+    try {
+        res.status(200).json(messages);
+    } catch (err) {
+        res.status(400).send('Unknown Error GET');
+    }
 };
 
 // POST Request Handler
 const addNewMessage = (req, res) => {
-    res.status(200).send('Successful API POST Request');
+    try {
+        messages.push(req.body);
+        res.status(200).json(messages);
+    } catch (err) {
+        res.status(400).send('Unknown Error POST');
+    }
 };
 
 export default { getAllMessages, addNewMessage };
